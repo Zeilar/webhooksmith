d7df96efd881063b7009802078bc0a1ac4ce6069
Philip-Homelab <homelab@angelin.dev>
2026-02-24 00:19:09 +0100

    feat: responsive style

--- apps/web/ui/components/page.tsx
+++ apps/web/ui/components/page.tsx
@@ -11,16 +11,7 @@ interface PageContainerProps extends PropsWithChildren {
 }
 
 export function PageShell({ children, className }: PageShellProps) {
-  return (
-    <main
-      className={classNames(
-        "h-full w-full bg-transparent px-4 text-slate-100 sm:px-6 lg:px-10 lg:py-6 xl:px-16",
-        className,
-      )}
-    >
-      {children}
-    </main>
-  );
+  return <main className={classNames("h-full w-full bg-transparent p-4 lg:p-8", className)}>{children}</main>;
 }
 
 export function PageContainer({ children, className, maxWidthClassName = "max-w-5xl" }: PageContainerProps) {
